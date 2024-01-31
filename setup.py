4c3e29118748e6ba53328b4e6c726974fa0b47a1
Gabriele <gabrielepaoletti@tiscali.it>
2024-01-31 18:31:21 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'A private collection of essential functions and models for streamlined machine learning experiments.',
+    'author': 'Gabriele Paoletti',
+    'url': 'https://github.com/gabrielepaoletti/utils',
+    'download_url': 'https://github.com/gabrielepaoletti/utils',
+    'author_email': 'gabriele.paoletti@uniroma1.it',
+    'version': '0.1',
+    'install_requires': ['matplotlib', 'numpy', 'pandas', 'pyproj'],
+    'packages': ['seismutils'],
+    'name': 'seismutils'
+}
+
+setup(**config)
