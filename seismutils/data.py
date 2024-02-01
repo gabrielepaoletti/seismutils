9b8207e1851fd61690ac0919572b98f041555113
Gabriele Paoletti <gabrielepaoletti@tiscali.it>
2024-02-01 11:29:53 +0100

    Updated data.py functions

--- seismutils/data.py
+++ seismutils/data.py
@@ -8,39 +8,36 @@ from typing import List, Tuple
 from matplotlib.ticker import MultipleLocator
 
 def cross_sections(data: pd.DataFrame,
-                   center: Tuple,
-                   num_sections: Tuple,
+                   center: Tuple[float, float],
+                   num_sections: Tuple[int, int],
                    event_distance_from_section: int,
                    strike: int,
-                   map_length: Tuple,
-                   depth_range: Tuple,
+                   map_length: int,
+                   depth_range: Tuple[int, int],
                    section_distance: int = 1,
-                   return_dataframes: bool=False) -> List[pd.DataFrame]:
+                   plot: bool=False,
+                   return_dataframes: bool=True) -> List[pd.DataFrame]:
     '''
-    Plots earthquake data in cross sections around a specified central point based on their orientation and position.
+    Analyzes and optionally plots earthquake data in cross sections around a specified central point, based on their orientation and position relative to a geological strike. 
 
     Args:
-        data (pd.DataFrame): DataFrame with columns 'lon', 'lat', 'depth', etc., containing earthquake event data.
-        center (tuple): Longitude and latitude of the central point (format: (longitude, latitude)) for the primary cross section.
-        num_sections (tuple): Tuple specifying the number of additional sections to be plotted around the primary section.
-                              Format: (num_left, num_right), where 'num_left' is the number of sections to the left (or before)
-                              the primary section, and 'num_right' is the number of sections to the right (or after) the primary section.
-        event_distance_from_section (int): Maximum distance (in kilometers) from a section within which an earthquake event is included.
-        strike (int): The strike angle, in degrees from North, of the geological structure being studied. The sections are plotted 
-                      perpendicular to this strike direction.
-        map_length (int): Length of the section's trace in kilometers, in both directions with respect to the center point.
-        depth_range (tuple): Depth range for considering earthquake events, specified as (min_depth, max_depth).
-        section_distance (int): Distance between adjacent sections, measured in kilometers. Default is 1 kilometer.
-        return_dataframes (bool, optional): If True, returns a list of DataFrames, one for each section. Default is False.
+        data (pd.DataFrame): DataFrame containing earthquake event data, with columns 'lon', 'lat', 'depth', etc.
+        center (Tuple[float, float]): Coordinates (longitude, latitude) of the central point for the primary cross section.
+        num_sections (Tuple[int, int]): Number of additional sections to be analyzed and plotted around the primary section, specified as (num_left, num_right).
+        event_distance_from_section (int): Maximum distance (in kilometers) from a section within which an earthquake event is considered for inclusion.
+        strike (int): Strike angle in degrees from North, indicating the geological structure's orientation. Sections are plotted perpendicular to this strike direction.
+        map_length (int): Length of the section's trace in kilometers, extending equally in both directions from the center point.
+        depth_range (Tuple[int, int]): Depth range (min_depth, max_depth) for considering earthquake events.
+        section_distance (int, optional): Distance between adjacent sections, in kilometers. Defaults to 1 kilometer.
+        plot (bool, optional): If True, plots the cross sections with the included earthquake events. Defaults to False.
+        return_dataframes (bool, optional): If True, returns a list of DataFrames, each representing a section with included earthquake events. Defaults to True.
 
     Returns:
-        list[pd.DataFrame] or None: If 'return_dataframes' is True, returns a list of DataFrames, each corresponding to a plotted section.
-                                    Returns None if 'return_dataframes' is False.
+        List[pd.DataFrame] or None: A list of DataFrames, each corresponding to a section with relevant earthquake data, if 'return_dataframes' is True. Returns None otherwise.
 
-    This function creates a visual representation of earthquake data by plotting cross sections. Each section is oriented
-    perpendicular to a specified strike angle and centered around a given point. Earthquake events are included in a section
-    if they fall within a specified distance from the section line and are within the defined depth range. The primary section
-    is centered at the given 'center', with additional sections plotted to the left and right based on the 'num_sections' parameter.
+    This function facilitates the analysis of earthquake events in relation to a specified geological strike. It computes cross sections perpendicular to the strike, centered 
+    around a given point, and analyzes earthquake events within these sections based on their proximity to the section plane and depth. The function optionally plots these sections, 
+    showing the spatial distribution of events, and can return the data in a structured format for further analysis.
     '''
     # Function to convert lat/lon to UTM
     def convert_to_utm(lon, lat):
@@ -81,85 +78,85 @@ def cross_sections(data: pd.DataFrame,
         # Calculate distance of events from each section plane and filter by depth
         dist = distance_point_from_plane(utmx, utmy, -data['depth'], normal_ref, center_coords[section])
         in_depth_range = (data['depth'] >= -depth_range[1]) & (data['depth'] <= -depth_range[0])
-        close_and_in_depth = np.where((dist < event_distance_from_section) & in_depth_range & (np.abs(utmx - center_coords[section][0]) < map_length))
-
-        on_section_coords =+ (utmy[close_and_in_depth] - center_coords[section][1]) * normal_ref[0] - (utmx[close_and_in_depth] - center_coords[section][0])*normal_ref[1]
+        on_section_coords =+ (utmy - center_coords[section][1]) * normal_ref[0] - (utmx - center_coords[section][0]) * normal_ref[1]
         
-        # Plot sections
-        fig = plt.figure(figsize=(15, 7))
+        close_and_in_depth = np.where((dist < event_distance_from_section) & in_depth_range & (np.abs(on_section_coords) < map_length))
         
-        plt.scatter(on_section_coords, -data.depth.iloc[close_and_in_depth], marker='.', color='black', s=0.25, alpha=0.75)
-        plt.title(f'Section {section+1}', fontsize=14, fontweight='bold')
-        
-        # Format plot axis
-        plt.gca().xaxis.set_major_locator(MultipleLocator(5))
-        plt.gca().xaxis.set_major_formatter('{x:.0f}')
-        plt.gca().xaxis.set_minor_locator(MultipleLocator(1))
-        
-        plt.gca().yaxis.set_major_locator(MultipleLocator(5))
-        plt.gca().yaxis.set_major_formatter('{x:.0f}')
-        plt.gca().yaxis.set_minor_locator(MultipleLocator(1))
-        
-        plt.gca().set_aspect('equal')
-        plt.xlabel('Distance along strike [km]', fontsize=12)
-        plt.ylabel('Depth [km]', fontsize=12)
-        plt.xlim(-map_length, map_length)
-        plt.ylim(*depth_range)
-        plt.show()
+        if plot:
+            # Plot sections
+            fig = plt.figure(figsize=(15, 7))
+            
+            plt.scatter(on_section_coords[close_and_in_depth], -data.depth.iloc[close_and_in_depth], marker='.', color='black', s=0.25, alpha=0.75)
+            plt.title(f'Section {section+1}', fontsize=14, fontweight='bold')
+            
+            # Format plot axis
+            plt.gca().xaxis.set_major_locator(MultipleLocator(5))
+            plt.gca().xaxis.set_major_formatter('{x:.0f}')
+            plt.gca().xaxis.set_minor_locator(MultipleLocator(1))
+            
+            plt.gca().yaxis.set_major_locator(MultipleLocator(5))
+            plt.gca().yaxis.set_major_formatter('{x:.0f}')
+            plt.gca().yaxis.set_minor_locator(MultipleLocator(1))
+            
+            plt.gca().set_aspect('equal')
+            plt.xlabel('Distance along strike [km]', fontsize=12)
+            plt.ylabel('Depth [km]', fontsize=12)
+            plt.xlim(-map_length, map_length)
+            plt.ylim(*depth_range)
+            plt.show()
         
         # Add the events of this section to the list if return_dataframes is True
         if return_dataframes:
             # Add on section coordinates to the dataframe
             section_df = data.iloc[close_and_in_depth].copy().reset_index(drop=True)
-            section_df['on_section_coords'] = on_section_coords
+            section_df['on_section_coords'] = on_section_coords[close_and_in_depth]
             
             # Append section dataframes to a list
             section_dataframes.append(section_df)
             
     return section_dataframes
 
-def select(coords: Tuple, 
-           center: Tuple, 
-           size: Tuple,
+def select(data: pd.DataFrame,
+           coords: Tuple[pd.Series, pd.Series], 
+           center: Tuple[float, float], 
+           size: Tuple[int, int],
            rotation: int,
-           shape_type: str) -> List:
+           shape_type: str,
+           plot: bool = False,
+           return_indices: bool=False) -> List:
     '''
-    Selects and returns indices of points from a tuple of coordinates that fall within a specified geometric 
-    shape (circle, oval, rectangle) around a central point, with an option to rotate the shape.
+    Selects indices of points from a set of coordinates that fall within a specified geometric shape (circle, oval, rectangle) centered at a given point. The shape can be rotated by 
+    a specified angle.
 
     Args:
-        coords (tuple of arrays): A tuple containing two arrays (x_coords, y_coords), representing the x and y 
-                                  coordinates of each point.
-        center (tuple): Coordinates of the center of the shape (format: (x, y)).
-        size (tuple or int): Size of the shape. For circles, it's a single integer representing the radius. For ovals 
-                             and rectangles, it's a tuple representing (width, height).
-        rotation (int): Rotation angle of the shape in degrees, counter-clockwise from the x-axis.
-        shape_type (str): Type of the geometric shape. Options are 'circle', 'oval', or 'rectangle'.
+        coords (Tuple[pd.Series, pd.Series]): A pair of Series representing x and y coordinates of points.
+        center (Tuple[float, float]): The x and y coordinates of the shape's center.
+        size (Tuple[int, int] or int): The dimensions of the shape. For circles, it's a single integer (radius); for ovals and rectangles, it's (width, height).
+        rotation (int): The counter-clockwise rotation angle of the shape in degrees from the x-axis.
+        shape_type (str): The type of geometric shape ('circle', 'oval', or 'rectangle').
+        plot (bool, optional): If True, plots the original points and the selected points. Defaults to False.
 
     Returns:
-        list: A list of indices of the points that fall within the specified shape.
+        List[int]: Indices of points within the specified shape.
 
-    This function iterates over each point in the input tuple of coordinates, transforming the coordinates of 
-    each point according to the specified rotation and shape center. It then checks if the transformed point falls 
-    within the given shape (circle, oval, or rectangle).
+    This function assesses each point to determine if it lies within a rotated geometric shape centered at a specified point. It supports selection within circles, ovals, and rectangles.
+    An optional plot can be generated to visualize the selection.
     '''
-    def rotate_point(point: Tuple,
-                     center: Tuple, 
+    def rotate_point(point: Tuple[float, float],
+                     center: Tuple[float, float], 
                      angle: int) -> tuple:
         '''
-        Rotates a point around a given center by a specified angle.
+        Rotates a point around a given center by a specified angle in the 2D plane.
 
         Args:
-            point (tuple): The coordinates of the point to be rotated (format: (x, y)).
-            center (tuple): The coordinates of the rotation center (format: (x, y)).
-            angle (int): The angle of rotation in degrees. Positive values rotate the point counter-clockwise, 
-                         while negative values rotate it clockwise.
+            point (Tuple[float, float]): Coordinates of the point (x, y) to be rotated.
+            center (Tuple[float, float]): Coordinates of the rotation center (x, y).
+            angle (int): Rotation angle in degrees, where positive values indicate counter-clockwise rotation.
 
         Returns:
-            tuple: The coordinates of the rotated point (format: (x, y)).
+            Tuple[float, float]: The coordinates of the rotated point (x, y).
 
-        This function calculates the new coordinates of a point after rotating it around a specified center. 
-        The rotation is performed in the 2D plane, and the angle is specified in degrees.
+        This function computes the new coordinates of a point after rotating it around a specified center point by a given angle in degrees.
         '''
         angle_rad = np.deg2rad(angle)
         ox, oy = center
@@ -191,4 +188,21 @@ def select(coords: Tuple,
                     center[1] - height/2 <= rotated_point[1] <= center[1] + height/2):
                 selected_indices.append(index)
 
-    return selected_indices
+    if plot:
+        fig = plt.figure(figsize=(15, 7))
+        plt.title(f'Selection', fontsize=14, fontweight='bold')
+        
+        plt.scatter(coords[0], coords[1] if coords[1].name != 'depth' else -coords[1], marker='.', color='grey', s=0.25, alpha=0.75)
+        plt.scatter(coords[0].iloc[selected_indices], coords[1].iloc[selected_indices] if coords[1].name != 'depth' else -coords[1].iloc[selected_indices], marker='.', color='blue', s=0.25, alpha=0.75)
+      
+        plt.gca().set_aspect('equal')
+        plt.xlabel(f'{coords[0].name}', fontsize=12)
+        plt.ylabel(f'{coords[1].name}', fontsize=12)
+        plt.xlim(coords[0].min(), coords[0].max())
+        plt.ylim(coords[1].max() if coords[1].name != 'depth' else -coords[1].max(), coords[1].min() if coords[1].name != 'depth' else -coords[1].min())
+        plt.show()
+    
+    if return_indices:  
+        return selected_indices
+    else:
+        return data.iloc[selected_indices]
