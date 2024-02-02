be00e6d7edd4121bd8ba7e099e599b4de088231f
Gabriele <gabrielepaoletti@tiscali.it>
2024-02-03 00:21:26 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ config = {
     'url': 'https://github.com/gabrielepaoletti/seismutils',
     'download_url': 'https://github.com/gabrielepaoletti/seismutils',
     'author_email': 'gabriele.paoletti@uniroma1.it',
-    'version': '0.1',
+    'version': '0.0.1',
     'python_requires': '>=3.6',
     'install_requires': ['matplotlib', 'numpy', 'pandas', 'pyproj'],
     'packages': find_packages(),
