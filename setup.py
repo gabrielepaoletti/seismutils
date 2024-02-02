d5279f32c45d02940c2be7b0581fbffb603791fb
Gabriele <gabrielepaoletti@tiscali.it>
2024-02-02 22:21:55 +0100

    General update

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 config = {
     'description': 'A private collection of essential functions and models for streamlined machine learning experiments.',
@@ -10,9 +7,12 @@ config = {
     'download_url': 'https://github.com/gabrielepaoletti/seismutils',
     'author_email': 'gabriele.paoletti@uniroma1.it',
     'version': '0.1',
+    'python_requires': '>=3.6',
     'install_requires': ['matplotlib', 'numpy', 'pandas', 'pyproj'],
     'packages': find_packages(),
-    'name': 'seismutils'
+    'name': 'seismutils',
+    'license': 'MIT',
+    'keywords': 'seismology earthquake geophysics data-analysis'
 }
 
 setup(**config)
