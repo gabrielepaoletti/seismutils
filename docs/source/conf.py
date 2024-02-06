314a9b0d80645d4e7f6f27819462987ae814699d
Gabriele <gabrielepaoletti@tiscali.it>
2024-02-06 23:51:47 +0100

    Upgraded to v0.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Gabriele Paoletti'
 author = 'Gabriele Paoletti'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
